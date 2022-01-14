3e8c41045bb8977421a8d2bf8118dc7dafcb931b
Michal Gorczynski <michal.gorczynski@thalesgroup.com>
2022-01-14 18:38:43 +0100

    After part 2

--- src/main/java/com/pluralsight/blog/data/CategoryRepository.java
+++ src/main/java/com/pluralsight/blog/data/CategoryRepository.java
@@ -3,19 +3,13 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 
-@Component
-public class CategoryRepository {
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    public List<Category> findAll() {
-        return null;
-    }
-
-    public Optional<Category> findById(Long id) {
-        return null;
-    }
 }
--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,10 +1,15 @@
 package com.pluralsight.blog.data;
 
+import com.pluralsight.blog.model.Category;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    public List<Post> findByCategory(Category category);
 
 }
